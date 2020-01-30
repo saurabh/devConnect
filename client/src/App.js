54fa9bd583bdd3656cfd3501c2cd8cdcdedd46d7
Saurabh W <w.saurabh@protonmail.com>
2020-01-30 23:32:34 +0400

    Added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from '../src/components/layout/Navbar';
 import Landing from '../src/components/layout/Landing';
-import Register from '../src/components/auth/Register';
-import Login from '../src/components/auth/Login';
-import Alert from '../src/components/layout/Alert';
-import PrivateRoute from '../src/components/routing/PrivateRoute';
-import Dashboard from '../src/components/dashboard/Dashboard';
-import CreateProfile from '../src/components/profile-forms/CreateProfile';
-import EditProfile from '../src/components/profile-forms/EditProfile';
-import AddExperience from '../src/components/profile-forms/AddExperience';
-import AddEducation from '../src/components/profile-forms/AddEducation';
-import Profiles from '../src/components/profiles/Profiles';
-import Profile from '../src/components/profile/Profile';
-import Posts from '../src/components/posts/Posts';
-import CreatePost from '../src/components/post-forms/CreatePost';
-import EditPost from '../src/components/post-forms/EditPost';
-import Post from '../src/components/post/Post';
+import Routes from '../src/components/routing/Routes';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -40,41 +26,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/profile/:id' component={Profile} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={EditProfile}
-              />
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/create-post' component={CreatePost} />
-              <PrivateRoute exact path='/edit-post/:id' component={EditPost} />
-              <PrivateRoute exact path='/posts/:id' component={Post} />
-            </Switch>
-          </section>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Routes component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
